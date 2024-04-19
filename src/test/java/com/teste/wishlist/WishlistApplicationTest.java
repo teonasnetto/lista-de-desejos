ada00681433fe506f39b442b21f1a16e00dccacb
Teonas <teonasnetto@gmail.com>
2024-04-19 18:07:52 -0300

    fix: main class test

--- src/test/java/com/teste/wishlist/WishlistApplicationTest.java
+++ src/test/java/com/teste/wishlist/WishlistApplicationTest.java
@@ -1,23 +1,10 @@
 package com.teste.wishlist;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 class WishlistApplicationTest {
-
     @Test
-    void testMain() {
-        String[] args = {};
-        ConfigurableApplicationContext context = SpringApplication.run(WishlistApplication.class, args);
-        assertNotNull(context);
-        context.close();
-    }
-
-    @Test
-    void mainApplication() {
+    void main() {
         WishlistApplication.main(new String[] {});
     }
 }
