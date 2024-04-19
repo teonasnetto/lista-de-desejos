28eaca302dde7c6780c66fe8c3dbd2a807a9ef1f
Teonas <teonasnetto@gmail.com>
2024-04-19 17:40:37 -0300

    feat: create Application main class test

---
+++ src/test/java/com/teste/wishlist/WishlistApplicationTest.java
@@ -0,0 +1,18 @@
+package com.teste.wishlist;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+class WishlistApplicationTest {
+
+    @Test
+    void testMain() {
+        String[] args = {};
+        ConfigurableApplicationContext context = SpringApplication.run(WishlistApplication.class, args);
+        assertNotNull(context);
+        context.close();
+    }
+}
